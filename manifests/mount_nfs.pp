d499eb6c573b72cd94773ebc6d947d23a065543b
Tommy McNeely <thomas.mcneely@davita.com>
2018-02-20 15:15:02 -0700

    Issue-1 Resource ordering and nfs_options on AIX

--- manifests/mount_nfs.pp
+++ manifests/mount_nfs.pp
@@ -1,18 +1,19 @@
 # == Class: fsutil::mount_nfs
 #
 # Mount an NFS share on a directory - make sure the directory exists
-# 
+#
 #
 define fsutil::mount_nfs(
   # parameters are populated from External(hiera)/Defaults/Fail
   String $device, # must be set - no defaults
-  String $options = 'defaults',
+  String $options = $fsutil::nfs_options,
   String $mode = '0755',
   String $umask = '0022',
 ){
     fsutil::mkmount { $name:
-      mode  => $mode,
-      umask => $umask,
+      mode   => $mode,
+      umask  => $umask,
+      before => Mount[$name],
     }
     mount { $name:
       ensure  => mounted,
